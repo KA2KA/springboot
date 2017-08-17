7d7ac007f9ffb15b51a30cd1242b66f3df7ee467
QIEGAO\QIEGAO <wuwanggao@163.com>
2017-08-18 00:55:30 +0800

    更新权限

---
+++ src/main/java/com/kaka/msystem/dao/SysPermissionMapper.java
@@ -0,0 +1,7 @@
+package com.kaka.msystem.dao;
+
+/**
+ * Created by QIEGAO on 2017/8/17.
+ */
+public class SysPermissionMapper {
+}
---
+++ src/test/java/com/kaka/msystem/dao/SysRoleMapperTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by QIEGAO on 2017/8/17.
+ */
+public class SysRoleMapperTest {
+
+}
