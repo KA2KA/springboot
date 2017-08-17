21340653cf75d345b232c3622b0e3e59f1c3e1f1
wuwg <wuwanggao@pinming.cn>
2017-08-17 16:46:14 +0800

    更新shiro

--- src/main/java/com/kaka/shiro/realm/JdbcAuthorizingRealm.java
+++ src/main/java/com/kaka/shiro/realm/JdbcAuthorizingRealm.java
@@ -1,42 +1,65 @@
 package com.kaka.shiro.realm;
 
+import com.kaka.msystem.dao.SysUserMapper;
 import com.kaka.msystem.model.SysMenu;
 import com.kaka.msystem.model.SysRole;
 import com.kaka.msystem.model.SysUser;
 import com.kaka.msystem.service.SysUserService;
+import com.kaka.utils.IPUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 /**
- * JDBC数据库链接校验
+ * 数据库方式登陆
  * Created by QIEGAO on 2017/8/10.
  */
-public class JDBCAuthRealm extends AuthorizingRealm {
+public class JdbcAuthorizingRealm extends AuthorizingRealm {
 
     @Autowired
     private SysUserService sysUserService;
+
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
 
     //认证.登录
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
         String username = token.getUsername();
-        SysUser user = sysUserService.findUserAndRoleByUserName(username);
-        return new SimpleAuthenticationInfo(user, user.getPassword(), this.getClass().getName());
+        if (StringUtils.isNotEmpty(username)) {
+            SysUser user = sysUserService.findUserAndRoleByUserName(username);
+            if (!user.isDelFlag()) {
+                throw new LockedAccountException("用户账号被禁用，请联系管理员！");
+            }
+            return new SimpleAuthenticationInfo(user, user.getPassword(), this.getClass().getName());
+        }
+        throw new AccountException("用户名不能为空");
     }
 
-    //授权
+
+    //授权.菜单
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principal) {
-        SysUser user = (SysUser) principal.fromRealm(this.getClass().getName()).iterator().next();//获取session中的用户
+        SysUser user = (SysUser) principal.fromRealm(this.getName()).iterator().next();//获取session中的用户
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String ipAddr = IPUtils.getIpAddr(request);
+        user.setIp(ipAddr);
+        sysUserMapper.updateSysUserIpById(user);
         List<String> permissions = new ArrayList<>();
         Set<SysRole> roles = user.getSysRoleSet();
         if (roles != null && !roles.isEmpty()) {
