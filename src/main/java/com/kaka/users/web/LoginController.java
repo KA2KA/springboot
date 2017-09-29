16d77901f46960097e31a4a4b224ecc7b7ff71c7
QIEGAO\QIEGAO <wuwanggao@163.com>
2017-09-29 08:02:13 +0800

    ==

--- src/main/java/com/kaka/users/web/LoginController.java
+++ src/main/java/com/kaka/users/web/LoginController.java
@@ -1,10 +1,12 @@
 package com.kaka.users.web;
 
+import com.kaka.common.utils.Result;
 import org.apache.shiro.util.Assert;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -15,8 +17,18 @@ import org.springframework.web.servlet.view.RedirectView;
  * @date 2017/9/28
  */
 @Controller
-@RequestMapping(value = "/users/")
+@RequestMapping(value = "/login")
 public class LoginController {
+    /**
+     * 登陆页面
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"})
+    public ModelAndView view() {
+        return new ModelAndView(new RedirectView("/login"));
+    }
+
 
     /**
      * 登录
@@ -25,14 +37,24 @@ public class LoginController {
      * @param password
      * @return
      */
-
-    @RequestMapping(value = "", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
-    public ModelAndView login(@RequestParam String username, @RequestParam String password) {
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    public Result login(@RequestParam String username, @RequestParam String password) {
         Assert.notNull(username, "username can not be empty");
         Assert.notNull(password, "password can not be empty");
 
-        return new ModelAndView(new RedirectView(""));
+        return Result.ok();
     }
 
+    /**
+     * 退出
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.DELETE, produces = {"application/json;charset=UTF-8"})
+    public ModelAndView loginOut() {
+
+        return new ModelAndView(new RedirectView(""));
+    }
 
 }
